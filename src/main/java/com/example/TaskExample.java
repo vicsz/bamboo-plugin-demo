630f0a44f51f91159c7014c5fe9fb68c3dc7a4bc
Victor Szoltysek <victor.szoltysek@flightnetwork.com>
2017-04-16 19:55:01 -0400

    Example of injecting beans in Bamboo Plugin development.

--- src/main/java/com/example/TaskExample.java
+++ src/main/java/com/example/TaskExample.java
@@ -1,20 +1,29 @@
 package com.example;
 
 import com.atlassian.bamboo.build.logger.BuildLogger;
-import com.atlassian.bamboo.task.TaskContext;
-import com.atlassian.bamboo.task.TaskException;
-import com.atlassian.bamboo.task.TaskResult;
-import com.atlassian.bamboo.task.TaskResultBuilder;
-import com.atlassian.bamboo.task.TaskType;
+import com.atlassian.bamboo.plan.PlanManager;
+import com.atlassian.bamboo.task.*;
+import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 
+@Scanned
 public class TaskExample implements TaskType
 {
+    @ComponentImport
+    private final PlanManager planManager;
+
+    public TaskExample(final PlanManager planManager)
+    {
+        this.planManager = planManager;
+    }
+
     @Override
     public TaskResult execute(final TaskContext taskContext) throws TaskException
     {
         final BuildLogger buildLogger = taskContext.getBuildLogger();
 
-        buildLogger.addBuildLogEntry("Hello, World!");
+        buildLogger.addBuildLogEntry("Hello, World!!!!");
+        buildLogger.addBuildLogEntry("Number of plans:" + planManager.getPlanCount());
 
         return TaskResultBuilder.create(taskContext).success().build();
     }

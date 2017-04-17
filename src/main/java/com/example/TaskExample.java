d89d0b6479f6efe9820e931b9e7e237bcdacaf58
Victor Szoltysek <victor.szoltysek@flightnetwork.com>
2017-04-16 22:08:54 -0400

    Extend TaskExample Logic ... add usage of Deployment Services to create and deploy a new release.

--- src/main/java/com/example/TaskExample.java
+++ src/main/java/com/example/TaskExample.java
@@ -1,30 +1,88 @@
 package com.example;
 
 import com.atlassian.bamboo.build.logger.BuildLogger;
-import com.atlassian.bamboo.plan.PlanManager;
+import com.atlassian.bamboo.deployments.environments.Environment;
+import com.atlassian.bamboo.deployments.execution.service.DeploymentExecutionService;
+import com.atlassian.bamboo.deployments.projects.DeploymentProject;
+import com.atlassian.bamboo.deployments.projects.service.DeploymentProjectService;
+import com.atlassian.bamboo.deployments.versions.DeploymentVersion;
+import com.atlassian.bamboo.deployments.versions.service.DeploymentVersionService;
 import com.atlassian.bamboo.task.*;
 import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+
+import java.util.List;
 
 @Scanned
 public class TaskExample implements TaskType
 {
     @ComponentImport
-    private final PlanManager planManager;
+    private final DeploymentVersionService deploymentVersionService;
 
-    public TaskExample(final PlanManager planManager)
+    @ComponentImport
+    private final DeploymentProjectService deploymentProjectService;
+
+    @ComponentImport
+    private final DeploymentExecutionService deploymentExecutionService;
+
+    public TaskExample(DeploymentVersionService deploymentVersionService, DeploymentProjectService deploymentProjectService, DeploymentExecutionService deploymentExecutionService)
     {
-        this.planManager = planManager;
+        this.deploymentVersionService = deploymentVersionService;
+        this.deploymentProjectService = deploymentProjectService;
+        this.deploymentExecutionService = deploymentExecutionService;
     }
 
     @Override
     public TaskResult execute(final TaskContext taskContext) throws TaskException
     {
+
         final BuildLogger buildLogger = taskContext.getBuildLogger();
 
-        buildLogger.addBuildLogEntry("Hello, World!!!!");
-        buildLogger.addBuildLogEntry("Number of plans:" + planManager.getPlanCount());
+        try {
 
-        return TaskResultBuilder.create(taskContext).success().build();
+            DeploymentProject deploymentProject = getMatchingDeploymentProject("DeploymentProject");
+
+            DeploymentVersion deploymentVersion = deploymentVersionService.getOrCreateDeploymentVersion(deploymentProject.getId(), taskContext.getBuildContext().getParentBuildContext().getPlanResultKey());
+
+            Environment environment = getMatchingEnvironment(deploymentProject, "QA");
+
+            deploymentExecutionService.prepareDeploymentContext(environment, deploymentVersion, taskContext.getBuildContext().getTriggerReason());
+
+            buildLogger.addBuildLogEntry(deploymentVersion.getName() + " deployment to " + environment + " triggered.");
+
+            return TaskResultBuilder.create(taskContext).success().build();
+
+        }
+        catch (Exception exception){
+
+            buildLogger.addErrorLogEntry(exception.getMessage());
+
+            return TaskResultBuilder.create(taskContext).failed().build();
+
+        }
+
     }
+
+    private DeploymentProject getMatchingDeploymentProject(String name){
+
+        List<DeploymentProject> allDeploymentProjects = deploymentProjectService.getAllDeploymentProjects();
+
+        for (DeploymentProject deploymentProject : allDeploymentProjects) {
+            if(deploymentProject.getName().equals(name))
+                return deploymentProject;
+        }
+
+        throw new RuntimeException("Unable to find deployment project: " + name);
+    }
+
+    private Environment getMatchingEnvironment(DeploymentProject deploymentProject, String name) {
+
+        for (Environment environment : deploymentProject.getEnvironments()) {
+            if(environment.getName().equals(name))
+                return environment;
+        }
+
+        throw new RuntimeException("Unable to find environment: " + name);
+    }
+
 }
